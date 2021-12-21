23cb4857e72ee0e9a297270a4160dcc34b0e6cba
Leandro-Barretoo <officialdevmi@gmail.com>
2021-12-21 06:18:47 -0800

    Fix rubocop offenses

--- person.rb
+++ person.rb
@@ -1,24 +1,23 @@
 #!/usr/bin/env ruby
 
 class Person
-    attr_accessor :name, :age
-    attr_reader :id, :name, :age
+  attr_accessor :name, :age
+  attr_reader :id
 
-    def constructor (name ='Unknown', age, parent_permission = 'true')
-        @id = Random.rand(1..1000)
-        @name = name
-        @age = age
-        @parent_permission = parent_permission
-    end
+  def initialize(age, name: 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
 
-    def can_use_services?
-        if (is_of_age? || parent_permission === true) return true
-    end
+  def can_use_services?
+    of_age? || @parent_permission
+  end
 
-    private
+  private
 
-    def is_of_age?
-        if (age >= 18) return true
-        return false
-    end
+  def of_age?
+    @age >= 18
+  end
 end
--- teacher.rb
+++ teacher.rb
@@ -1,8 +1,8 @@
 require_relative './person'
 
 class Teacher < Person
-  def constructor(specialization)
-    super
+  def initialize(specialization, age, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
     @specialization = specialization
   end
 
